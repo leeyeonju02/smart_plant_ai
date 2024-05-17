763d587f08aaa45bff5790a69c0146b1ad4c1737
leeyeonju02 <dlduswn0783@naver.com>
2024-05-18 01:30:39 +0900

    랭체인 API 추가

--- router.py
+++ router.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/create/', methods=['POST'])
+def create(): 
+    plantData = request.get_json() 
 
-if __name__ == '__main__':
-    app.run()
+    print(type(plantData))
+    return jsonify({"message": "식물데이터를 성공적으로 받았습니다. "})
+
+@app.route('/chat/', methods=['POST'])
+def chat(): 
+    chatData = request.get_json()
+    print(type(chatData))
+    print(chatData)
+    return jsonify({"open_ai_message": "답변 도착"})
+
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
