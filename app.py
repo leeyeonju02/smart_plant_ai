325ea2bc88b463f7aced7882b0bc59d0b34d8971
Isonade2 <c1004sos@naver.com>
2024-05-18 22:12:15 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -6,21 +6,16 @@ app = Flask(__name__)
 CORS(app)
 
 
-
-@app.route('/create/', methods=['POST'])
-def create(): 
-    plantData = request.get_json() 
-
-    print(type(plantData))
-    return jsonify({"message": "식물데이터를 성공적으로 받았습니다. "})
-
-@app.route('/chat/', methods=['POST'])
-def chat(): 
+@app.route("/chat/", methods=["POST"])
+def chat():
     chatData = request.get_json()
     print(type(chatData))
     print(chatData)
+
+    doSomeThing(chatData)
+
     return jsonify({"open_ai_message": "답변 도착"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
