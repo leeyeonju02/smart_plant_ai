03e397dcb90eb1169dcc06473f39f700ee6e21ab
Isonade2 <c1004sos@naver.com>
2024-05-18 22:11:39 +0900

    정보 받아오는 api 삭제 /chat api로 통합

---
+++ 답변 형식.py
@@ -0,0 +1,29 @@
+{
+    "message": "321",
+    "plantData": {
+        "message": "식물과 식물 기록 정보입니다.",
+        "content": {
+            "plant": {
+                "id": 1,
+                "name": "알라의 상추",
+                "exp": 0,
+                "level": 1,
+                "plantType": "상추",
+                "uuid": "5b44da51-62bb-4aa3-8502-4627d3a2354c",
+                "giveWater": False,
+                "createDate": "2024-05-18",
+            },
+            "plantHistory": {
+                "id": 20,
+                "plantId": 1,
+                "temp": 21,
+                "humidity": 69,
+                "soilHumidity": 310,
+                "remainingWater": 2128,
+                "gaveWater": False,
+                "light": 0,
+                "createdDate": "2024-05-18T20:57:15.976636",
+            },
+        },
+    },
+}
